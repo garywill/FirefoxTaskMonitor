9c19f470d3defdcc45f281fda009964b3cb9b191
garywill <garywill@disroot.org>
2026-07-26 09:40:48 +0800

    add warn to not edit the dist file

--- include.py
+++ include.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 
-def include_files(input_file, output_file):  
+import sys
+
+def include_files(input_file):  
     with open(input_file, 'r') as f:  
         lines = f.readlines()  
   
-    with open(output_file, 'w') as out:  
-        for line in lines:  
-            if line.strip().startswith('#include "'):  
-                include_file = line.split('"')[1]
-                with open('src/'+include_file, 'r') as inc:  
-                    out.write(inc.read())  
-            else:  
-                out.write(line)  
+    for line in lines:  
+        if line.strip().startswith('#include "'):  
+            include_file = line.split('"')[1]
+            with open('src/'+include_file, 'r') as inc:  
+                sys.stdout.write(inc.read())  
+        else:  
+            sys.stdout.write(line)  
   
 # 使用示例  
-include_files('src/part1_merger.uc.js', '/dev/stdout') 
+include_files('src/part1_merger.uc.js') 
